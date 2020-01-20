5453b9f06fa80b8d10edcedb940929377c7cd8a2
leecason <leecason0722@gmail.com>
2020-01-20 13:45:10 +0800

    chore: 🚧 init

---
+++ .vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: '个人主页',
+  description: '李嘉迅的博客',
+  themeConfig: {
+    sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+    lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
+  }
+};
