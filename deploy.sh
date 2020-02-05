4de8ac4e8e59bc305feace99faaa81b320a00c34
leecason <leecason0722@gmail.com>
2020-02-05 21:36:33 +0800

    chore: 🔧 deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy 🎉'
+
+# 如果发布到 https://<USERNAME>.github.io
+git push -f https://github.com/Leecason/leecason.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
