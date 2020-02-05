271a0113443d955f3a6acd34c23621220d1d28b8
leecason <leecason0722@gmail.com>
2020-02-05 17:26:45 +0800

    chore: 🍱 favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
   description: 'Talk is cheap. Show me the code.',
   locales: { '/': { lang: 'zh' } },
   head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
   ],
   theme: 'reco',
